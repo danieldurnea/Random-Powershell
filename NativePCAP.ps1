970306d3bc66d6670baa4221718e4b2663eb4814
SnipSnap (Harrison K) <70385098+SnipSnapp@users.noreply.github.com>
2024-02-13 13:59:44 -0600

    Update NativePCAP.ps1

--- NativePCAP.ps1
+++ NativePCAP.ps1
@@ -1,6 +1,6 @@
 #Take a PCAP without installing some stupid 3rd party program
 #Fill out the portions in brackets manually.
-pktmon start --etw --pkt-size 0 --f <FILENAME> -m multi-file -c
+pktmon start --etw --pkt-size 0 -f <FILENAME> -m multi-file -c
 
 #<COMMANDS that do some network thing>
 
